70e5b1124d97bdd292ff0612bfb079077623a5e5
pmaier1971 <pmaier1971@gmail.com>
2014-04-15 11:35:36 +0200

    - Automated data import

--- server.R
+++ server.R
@@ -36,28 +36,74 @@ misc.funggcast<-function(dn,fcast){
 
 shinyServer(function(input, output) {
   
-  # Data import
-  attach(NULL, name="DATA.ENV")
+  # Test whether we are online
   
-  List.Countries <- c("US", "Eurozone", "UK", "Canada", "China")
-  
-  getSymbols('GDPC1',src='FRED', env=as.environment("DATA.ENV"))
-  getSymbols('NAEXKP01EZQ652S',src='FRED', env=as.environment("DATA.ENV"))
-  getSymbols('NAEXKP01GBQ652S',src='FRED', env=as.environment("DATA.ENV"))
-  getSymbols('CANRGDPQDSNAQ',src='FRED', env=as.environment("DATA.ENV"))
-  getSymbols('RGDPNACNA666NRUG',src='FRED', env=as.environment("DATA.ENV"))
-  
-  
-  
-  List.Series <- ls("DATA.ENV")
-  #i<-1
-  for (i in 1:length(List.Countries)){
-    Country <- List.Countries[i]
-    Data.Name <- paste(Country,".GDP.qq", sep="")
-    FC.Name   <- paste(Country,".GDP.qq.FC", sep="")
-    Chart.Name<- paste(Country,".GDP.qq.FC.chart", sep="")
+  test <- try(getSymbols("GDPC1",src='FRED'))
+  if (inherits(test, "try-error")) load(file="InputData.RData") else {
+    rm("GDPC1")
     
-    Data.Series <- get(List.Series[i])
+    # Data import
+    cat("\nDownloading data online....")
+    misc.Import.Series <- function(Identifier, series.name, src="FRED"){
+      cat(paste("\n    >>> ", series.name))
+      getSymbols(series.name,src='FRED') #, env=as.environment("DATA.ENV"))
+      assign(Identifier, get(series.name))
+      return(Identifier)
+    }
+    
+    # attach(NULL, name="DATA.ENV")
+    assign("US.GDP.Real", get(getSymbols("GDPC1",src='FRED')))
+    assign("EU.GDP.Real", get(getSymbols("NAEXKP01EZQ652S",src='FRED')))
+    assign("UK.GDP.Real", get(getSymbols("NAEXKP01GBQ652S",src='FRED')))
+    assign("CN.GDP.Real", get(getSymbols("CANRGDPQDSNAQ",src='FRED')))
+    assign("CA.GDP.Real", get(getSymbols("RGDPNACNA666NRUG",src='FRED')))
+    
+    assign("US.Claims", get(getSymbols("IC4WSA",src='FRED')))
+    assign("US.Payroll", get(getSymbols("PAYEMS",src='FRED')))
+    assign("US.Unemployment", get(getSymbols("UNRATE",src='FRED')))
+    assign("EU.Unemployment", get(getSymbols("LRHUTTTTEZM156S",src='FRED')))
+    assign("US.CPI.Headline", get(getSymbols("CPIAUCSL",src='FRED')))
+    assign("US.CPI.Core", get(getSymbols("CPILFESL",src='FRED')))
+    assign("CA.CPI.Headline", get(getSymbols("CANCPIALLMINMEI",src='FRED')))
+    assign("CA.CPI.Core", get(getSymbols("CANCPICORMINMEI",src='FRED')))
+    
+    assign("EU.CPI.Headline", get(getSymbols("CP0000EZ17M086NEST",src='FRED')))
+    assign("EU.CPI.Headline2", get(getSymbols("CPHPTT01EZM661N",src='FRED')))
+    assign("EU.CPI.Core", get(getSymbols("CPHPLA01EZM661N",src='FRED')))
+    assign("US.IP", get(getSymbols("INDPRO",src='FRED')))
+    assign("US.SOV.10Y", get(getSymbols("DGS10",src='FRED')))
+    assign("EU.SOV.10Y", get(getSymbols("IRLTLT01EZM156N",src='FRED')))
+    
+    assign("FX.USDEUR", get(getSymbols("DEXUSEU",src='FRED')))
+    assign("FX.EUR.Effective", get(getSymbols("RBXMBIS",src='FRED')))
+    assign("FX.CADUSD", get(getSymbols("EXCAUS",src='FRED')))
+    assign("FX.CA.Effective", get(getSymbols("RBCABIS",src='FRED')))
+    
+    assign("US.Survey.Empire", get(getSymbols("GACDINA066MNFRBNY",src='FRED')))
+    assign("EU.Survey.ConsumerConfidence.Expectations", get(getSymbols("CSESFT02EZM460S",src='FRED')))
+    assign("EU.Survey.ConsumerConfidence", get(getSymbols("CSCICP02EZM460S",src='FRED')))
+    assign("EU.Survey.ManufacturingConfidence", get(getSymbols("BSCICP02EZM460S",src='FRED')))
+    assign("EU.Survey.CapacityUtilization", get(getSymbols("BSCURT02EZQ160S",src='FRED')))
+    
+  
+    #List.Series    <- ls("DATA.ENV")
+    List.Variables <- ls()    
+    
+    cat("\n.... saving a new RData file")
+    save(list=ls(), file="InputData.RData")
+  }
+  
+  List.Countries <- c("US", "EU", "UK", "CN", "CA")
+  
+  browser()
+  
+  for (i in 1:length(List.Countries)){
+    Country     <- List.Countries[i]
+    Data.Name   <- paste(Country,".GDP.qq", sep="")
+    FC.Name     <- paste(Country,".GDP.qq.FC", sep="")
+    Chart.Name  <- paste(Country,".GDP.qq.FC.chart", sep="")
+
+    Data.Series <- get(paste(Country, ".GDP.Real", sep=""))
     if (Country == "China") Data.Series <- 100*log(Data.Series / lag(Data.Series, 1))
     else Data.Series <- 400*log(Data.Series / lag(Data.Series, 1))
     
@@ -78,7 +124,14 @@ shinyServer(function(input, output) {
     FC.Name <- paste(input$Macro.Control.Choice, ".GDP.qq.FC", sep="")
     return( plot(forecast(get(FC.Name), h=4)))
   })
-  
+
+  output$GDP.Comparison.History <- renderPlot({
+    for (i in 1:length(List.Countries)){
+      Country     <- List.Countries[i]
+      Data.Name   <- paste(Country,".GDP.qq", sep="")
+    }
+      
+  })
   
   # --------- STOCK MARKET
   
